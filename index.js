74ae2dd03378d33f0f91dc322f345bd53a4293dd
Josh Abernathy <josh@github.com>
2018-07-02 15:20:06 -0400

    log again

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const main = app => {
-  app.on("pull_request.opened", async context => {
-    console.log(`hey there ${context}`)
-    app.log(`hey there ${context}!!`)
-  })
+  console.log('here!')
+
+  // app.on("pull_request.opened", async context => {
+  //   console.log(`hey there ${context}`)
+  //   app.log(`hey there ${context}!!`)
+  // })
 }
 
 module.exports.main = main
